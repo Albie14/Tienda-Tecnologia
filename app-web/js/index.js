6945a1bdf097f1746d4b02b30fad41f036f99901
Albie <albiesilva@gmail.com>
2025-09-27 13:36:30 -0400

    transicion entre vistas

--- app-web/js/index.js
+++ app-web/js/index.js
@@ -13,31 +13,66 @@
 //     })
 
 document.addEventListener("DOMContentLoaded", () => {
-  const enlaces = document.querySelectorAll("a[data-route]");
-  const vistas = document.querySelectorAll(".vista");
 
-  function navegar(ruta) {
-    // ocultar todas
-    vistas.forEach(vista => vista.classList.remove("active"));
+    const enlaces = document.querySelectorAll("a[data-route]");
+    const vistas = document.querySelectorAll(".vista");
 
-    // mostrar la que coincide con data-route
-    const destino = document.querySelector(`.vista[data-route="${ruta}"]`);
-    if (destino) {
-      destino.classList.add("active");
-    } else {
-      console.warn("Ruta no encontrada:", ruta);
+    function navegar(ruta){
+        if(document.startViewTransition){
+            document.startViewTransition(()=>{
+                cambiarVista(ruta)
+            })
+        }else{
+                cambiarVista(ruta)
+        }
     }
-  }
 
-  // interceptar clicks en los <a>
-  enlaces.forEach(enlace => {
-    enlace.addEventListener("click", e => {
-      e.preventDefault(); // evitar que recargue
-      const ruta = enlace.getAttribute("data-route");
-      navegar(ruta);
-    });
-  });
+    function cambiarVista(ruta){
+        vistas.forEach(vista=> {
+            vista.classList.remove("active");
+            vista.style.viewTransitionName = "none";
+        
+        });
 
-  // iniciar en /inicio
-  navegar("/inicio");
+        const rutaDestino = document.querySelector(`.vista[data-route="${ruta}"]`);
+        if(rutaDestino){
+            rutaDestino.classList.add("active");
+            rutaDestino.style.viewTransitionName = "vista";
+        }
+    }
+
+    enlaces.forEach(enlace=>{
+        enlace.addEventListener('click', e=>{
+            e.preventDefault();
+            const ruta = enlace.getAttribute("data-route");
+            navegar(ruta)
+        })
+    })
+
+    navegar("/inicio")
+    // function navegar(ruta) {
+    //     // ocultar todas
+    //     vistas.forEach(vista => vista.classList.remove("active"));
+
+    //     // mostrar la que coincide con data-route
+    //     const destino = document.querySelector(`.vista[data-route="${ruta}"]`);
+    //     if (destino) {
+    //     destino.classList.add("active");
+    //     } else {
+    //     console.warn("Ruta no encontrada:", ruta);
+    //     }
+    // }
+    // interceptar clicks en los <a>
+    // enlaces.forEach(enlace => {
+    //     enlace.addEventListener("click", e => {
+    //     e.preventDefault(); // evitar que recargue
+    //     const ruta = enlace.getAttribute("data-route");
+    //     navegar(ruta);
+    //     });
+    // });
+
+    // iniciar en /inicio
+    // navegar("/inicio");
+
+   
 });
